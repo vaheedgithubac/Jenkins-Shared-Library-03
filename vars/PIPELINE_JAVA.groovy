9c39e6c62f0652ebfcc1bcfaf1f05c197d35c2e0
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-02 18:57:29 +0530

    Update PIPELINE_JAVA.groovy

--- vars/PIPELINE_JAVA.groovy
+++ vars/PIPELINE_JAVA.groovy
@@ -17,7 +17,7 @@ def call(Map config = [:]) {
 	   }
 
 	   stages {
-	   		stage('Init') { steps {cleanWs()  // always clean } }
+	   		stage('Init') { steps {cleanWs() } }
 	   		stage("SET AND PRINT LATEST COMMIT ID") {
 	   			steps {
 					script {
