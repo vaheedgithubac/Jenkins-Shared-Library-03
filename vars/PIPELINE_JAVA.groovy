9b2684bc75a719f7678d686783413f6e9912283d
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-02 14:57:41 +0530

    Update PIPELINE_JAVA.groovy

--- vars/PIPELINE_JAVA.groovy
+++ vars/PIPELINE_JAVA.groovy
@@ -24,8 +24,9 @@ def call(Map config = [:]) {
 	   		stage("SET AND PRINT LATEST COMMIT ID") {
 	   			steps {
 					script {
-	   					env.MY_GIT_LATEST_COMMIT_ID = getLatestCommitIdShort()
-	   					echo "MY_GIT_LATEST_COMMIT_ID: ${env.MY_GIT_LATEST_COMMIT_ID}"	
+	   					//env.MY_GIT_LATEST_COMMIT_ID = getLatestCommitIdShort() ( To get this work, you should not declare a variable under pipeline environment{} block )
+						MY_GIT_LATEST_COMMIT_ID = getLatestCommitIdShort()
+	   					echo "MY_GIT_LATEST_COMMIT_ID: ${MY_GIT_LATEST_COMMIT_ID}"	
 					}
 	   			}
 	   		}
